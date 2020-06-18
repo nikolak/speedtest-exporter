9a6339b3b4afbb92922f21e1a2421d1498061322
nikolak <nikola@nikolak.com>
2020-06-18 21:56:24 +0200

    Add travisci integration and tests

--- tests.py
+++ tests.py
@@ -4,7 +4,6 @@ import requests
 def test():
     for line in requests.get("http://localhost:9102/metrics").text.splitlines():
         if line.startswith("speedtest_"):
-            print(line, float(line.split()[1]))
             assert float(line.split()[1]) > 0
 
 
